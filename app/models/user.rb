add0bf58c85f0515bdb2733f9c3d11f38e3d5a75
Kevin Cotrina <kcotrinam92@gmail.com>
2020-12-09 10:46:25 -0500

    Add Validation for friendship table

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,12 +10,14 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :friendships
-  has_many :inverse_friendships, class_name: 'Friendship', foreign_key: 'friend_id'
+  # has_many :inverse_friendships, class_name: 'Friendship', foreign_key: 'friend_id'
+  has_many :pending_invitation, -> { where confirmed: false }, class_name: 'Friendship', foreign_key: 'friend_id'
 
   def friends
-    friends_array = friendships.map{|friendship| friendship.friend if friendship.confirmed}
-    friends_array + inverse_friendships.map{|friendship| friendship.user if friendship.confirmed}
-    friends_array.compact
+    # friends_array = friendships.map{|friendship| friendship.friend if friendship.confirmed}
+    # friends_array + pending_invitation.map{|friendship| friendship.user if friendship.confirmed}
+    # friends_array.compact
+
   end
 
   # Users who have yet to confirme friend requests
