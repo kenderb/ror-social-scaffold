9b6e9dd6f3b36174c418817f2d4bb4b56a36d8ca
kenderb <kenderbolivar@gmail.com>
2020-12-09 12:52:05 -0500

    Create methods for friendships

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,7 +1,6 @@
 class Friendship < ApplicationRecord
-  belongs_to :user, foreign_key: 'user_id'
-  belongs_to :user, foreign_key: 'friend_id'
-  belongs_to :friendship_sender, class_name: 'User', foreign_key: 'user_id'
+  belongs_to :user
+  belongs_to :friend, class_name: 'User'
 
   validates :user_id, uniqueness: { scope: :friend_id, meesage: 'You have an invitation pending or are already friends' }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,13 +11,12 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :friendships
   # has_many :inverse_friendships, class_name: 'Friendship', foreign_key: 'friend_id'
-  has_many :pending_invitation, -> { where confirmed: false }, class_name: 'Friendship', foreign_key: 'friend_id'
+  has_many :pending_friendships, -> { where confirmed: false }, class_name: 'Friendship', foreign_key: 'friend_id'
 
   def friends
-    # friends_array = friendships.map{|friendship| friendship.friend if friendship.confirmed}
-    # friends_array + pending_invitation.map{|friendship| friendship.user if friendship.confirmed}
-    # friends_array.compact
-
+    friends_array = friendships.map{|friendship| friendship.friend if friendship.confirmed}
+    friends_array + pending_friendships.map{|friendship| friendship.user if friendship.confirmed}
+    friends_array.compact
   end
 
   # Users who have yet to confirme friend requests
@@ -27,13 +26,22 @@ class User < ApplicationRecord
 
   # Users who have requested to be friends
   def friend_requests
-    inverse_friendships.map{|friendship| friendship.user if !friendship.confirmed}.compact.uniq
+    pending_friendships.map{|friendship| friendship.user if !friendship.confirmed}.compact.uniq
   end
 
   def confirm_friend(user)
-    friendship = inverse_friendships.find{|friendship| friendship.user == user}
-    friendship.confirmed = true
-    friendship.save
+    received_invitation = pending_friendships.where(user_id: user.id).first
+    return if received_invitation.nil?
+    received_invitation.confirmed = true
+    if received_invitation.valid?
+      received_invitation.save
+    end
+
+    received_invitation2 = user.pending_friendships.where(user_id: id).first
+    received_invitation2.confirmed = true
+    if received_invitation2.valid?
+      received_invitation2.save
+    end
   end
 
   def friend?(user)
