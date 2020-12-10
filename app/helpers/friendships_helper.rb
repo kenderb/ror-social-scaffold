81eab533ec1eb599cd6a61282f0db8fe65c800bf
kcotrinam <kcotrinam@gmail.com>
2020-12-10 14:28:35 -0500

    refactor friend_status in friendships_helper

--- app/helpers/friendships_helper.rb
+++ app/helpers/friendships_helper.rb
@@ -1,10 +1,9 @@
 module FriendshipsHelper
   def friend_status(user)
     return if user == current_user
-    if !current_user.friendships.where(friend_id: user.id, confirmed: false ).empty? || !user.friendships.where(friend_id: current_user.id, confirmed: false ).empty?
+    if current_user.pending_friends.include?(user) || user.pending_friends.include?(current_user)
       render 'friendships/pendind_friend_request'
-    elsif !current_user.friendships.where(friend_id: user.id, confirmed: true ).empty? || !user.friendships.where(friend_id: current_user.id, confirmed: true ).empty?
-      
+    elsif current_user.friend?(user) || user.friend?(current_user)     
       render 'friendships/already_friends'
     else
       render 'friendships/add_friend_button', user: user
