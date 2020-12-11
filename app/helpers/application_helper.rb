f68c860f73826022f3d247486832fb82822b1e0a
kenderb <kenderbolivar@gmail.com>
2020-12-11 10:27:41 -0500

    add some links to the new navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
   def menu_link_to(link_text, link_path)
-    class_name = current_page?(link_path) ? 'menu-item active' : 'menu-item'
+    class_name = current_page?(link_path) ? 'navbar-item  is-uppercase' : 'navbar-item  is-uppercase'
 
     content_tag(:div, class: class_name) do
-      link_to link_text, link_path
+      link_to link_text, link_path, class: 'has-text-info has-text-weight-bold'
     end
   end
 
@@ -16,11 +16,11 @@ module ApplicationHelper
     end
   end
 
-  # def friend_status(user)
-  #   return if user == current_user
-  #   link_to 'Add Friend',
-  #           friendships_path(friendship: { user_id: current_user, friend_id: user.id, confirmed: false }),
-  #           method: :post, class: 'add-friend '
+  def friend_status(user)
+    return if user == current_user
+    link_to 'Add Friend',
+            friendships_path(friendship: { user_id: current_user, friend_id: user.id, confirmed: false }),
+            method: :post, class: 'add-friend '
 
-  # end
+  end
 end
