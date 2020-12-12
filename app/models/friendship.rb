d0f30f6ee55f92ecd938a7f6b0b28ef22e224fea
Kevin Cotrina <kcotrinam92@gmail.com>
2020-12-11 22:16:03 -0500

    Show current user posts and friends posts

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,7 +1,4 @@
 class Friendship < ApplicationRecord
   belongs_to :user
   belongs_to :friend, class_name: 'User'
-
-  validates :user_id,
-            uniqueness: { scope: :friend_id, meesage: 'You have an invitation pending or are already friends' }
 end
